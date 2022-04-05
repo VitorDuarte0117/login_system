68f80fdb7191367c374c179a2ab86da887dcc020
Vitor Duarte <91388189+VitorDuarte0117@users.noreply.github.com>
2022-04-05 14:33:24 -0300

    Add files via upload

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -2,19 +2,23 @@ import { useState } from "react";
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProtectedLayout } from "./components/ProtectedLayout";
+import { Login } from "./components/Login";
 
 function App() {
     return (
         <AuthProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/profile">
-                        <ProtectedLayout>
-                            <h2>Hello,This is the profile component!</h2>
-                        </ProtectedLayout>
-                    </Route>
+                    <Route
+                        path="/profile"
+                        element={
+                            <ProtectedLayout>
+                                <h2>Hello,this is the profile component</h2>
+                            </ProtectedLayout>
+                        }
+                    />
 
-                    <Route path="/login" />
+                    <Route path="/login" element={<Login />}></Route>
                 </Routes>
             </BrowserRouter>
         </AuthProvider>
