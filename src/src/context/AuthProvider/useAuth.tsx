a7ab27e4ebe8223891618635bafecb70dd1d096f
Vitor Duarte <91388189+VitorDuarte0117@users.noreply.github.com>
2022-04-05 14:34:55 -0300

    Delete src directory

--- src/src/context/AuthProvider/useAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from ".";
-
-//When another component need to use the context, just call using the "useAuth"
-export const useAuth = () => {
-    const context = useContext(AuthContext);
-
-    return context;
-};
